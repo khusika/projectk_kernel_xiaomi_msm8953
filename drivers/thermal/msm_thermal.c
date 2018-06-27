3ade7a2825f89f49319791f7a6257cc1cb27619f
khusika <khusikadhamar@gmail.com>
2018-06-27 12:55:04 +0700

    Revert "msm: thermal: Check temperature only if probed"
    This reverts commit b3e88e3cdec774a0f8092ce4d658ad569d10827a.
    
    Signed-off-by: khusika <khusikadhamar@gmail.com>

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -3529,9 +3529,6 @@ static void check_temp(struct work_struct *work)
 	long temp = 0;
 	int ret = 0;
 
-	if (!msm_thermal_probed)
-		return;
-
 	do_therm_reset();
 	if (!msm_thermal_probed)
  		return;
